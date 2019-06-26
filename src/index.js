c55caac307145a6584dc58bb2e47dcc3f7b7a824
Devin Sherman <fidalgo15@Devins-MacBook-Pro.local>
2019-06-25 19:58:02 -0600

    senators

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+let senators = require('./senators.json');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const party = senators.filter(party => party.party == 'republican', 'democrat', 'independent');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const state = senators.filter(state => state.state_name === 'Utah');
+
+const gender = senators.filter(gender => gender.gender == 'male', 'female');
+
+const simplify = state.map(state => {
+    return {
+        first_name:state.first_name,
+        last_name:state.last_name,
+        party:state.party,
+        gender:state.gender,
+    }
+});
+
+const senatorSenior = simplify.reduce((acc,cur) => acc);
+console.log(senatorSenior)
